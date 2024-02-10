6108a6020a7420beb0ba66420dc48ef932e3dc42
vikas yadav <thejsdeveloper@gmail.com>
2024-02-10 21:47:05 +0530

    feat: adding error page to handle error boundary

---
+++ src/app/error.tsx
@@ -0,0 +1,31 @@
+"use client"; // Error components must be Client Components
+
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <h2>Something went wrong!</h2>
+      <button
+        className="btn bg-linear  text-white w-full md:w-9/12 p-2 rounded-full mt-5"
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </button>
+    </div>
+  );
+}
