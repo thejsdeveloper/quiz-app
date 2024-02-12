b86aa6e46f4f8e00c4aaf76830db9684a4197d5e
vikas yadav <thejsdeveloper@gmail.com>
2024-02-12 13:41:02 +0530

    feat: layout min height

--- src/app/questions/playground/index.tsx
+++ src/app/questions/playground/index.tsx
@@ -56,7 +56,8 @@ function Playground() {
 
   return (
     <section
-      className="flex flex-col 
+      className="flex flex-col
+        min-h-[80vh]
         px-4 md:px-6 lg:px-8 
         py-6 md:py-12 rounded-lg text-white bg-blue-gray w-full md:w-2/3"
     >
